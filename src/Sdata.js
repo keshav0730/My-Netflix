dfd5d677ba5a21f6cc3ce204ed18548e53f0e2f0
keshav <kumarmishra0301@gmail.com>
2021-07-23 12:07:10 +0530

    change all the design and improve Row Website

---
+++ src/Sdata.js
@@ -0,0 +1,61 @@
+const Sdata = [
+    {
+        sname:"DARK",
+        imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+        title:"A Netflix Orignal Series",
+        links:"https://www.netflix.com/in/title/80100172"
+    },
+    {
+        sname:"Stranger Things",
+        imgsrc:"https://wallpapercave.com/wp/wp1917154.jpg",
+        title:"A Netflix Orignal Series",
+        links:"https://www.netflix.com/in/title/80057281"
+
+    },
+
+    {
+        sname:"The Vampire Diaries",
+        imgsrc:"https://wallpapercave.com/wp/d8wldxe.jpg",
+        title:"A Netflix Orignal Series",
+
+        links:"https://www.netflix.com/in/title/70143860"
+    },
+    {
+        sname:"Money -Heist",
+        imgsrc:"https://wallpapercave.com/wp/wp6126886.jpg",
+        title:"A Netflix Orignal Series",
+
+        links:"https://www.netflix.com/in/title/80192098"
+    },
+    {
+        sname:"Lucifer",
+        imgsrc:"https://wallpapercave.com/wp/wp6756794.jpg",
+        title:"A Netflix Orignal Series",
+
+        links:"https://www.netflix.com/in/title/80057918"
+    },
+    {
+        sname:"Twilight",
+        imgsrc:"https://wallpapercave.com/uwp/uwp710806.jpeg",
+        title:"A Netflix Orignal Series",
+
+        links:"https://www.netflix.com/title/70099113"
+    },
+    {
+        sname:"Vikings",
+        imgsrc:"https://wallpapercave.com/wp/wp3577513.jpg",
+        title:"A Netflix Orignal Series",
+
+        links:"https://www.netflix.com/in/title/70301870"
+    },
+    {
+        sname:"The Witcher",
+        imgsrc:"https://wallpapercave.com/wp/wp1854641.jpg",
+        title:"A Netflix Orignal Series",
+
+        links:"https://www.netflix.com/in/title/80189685"
+    }
+    
+];
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import Card from './Card'
+import "./index.css";
+import Sdata from "./Sdata";
+
 
 
 ReactDOM.render(
   <>
-  <h1>hello and welcome to</h1>
-  </>,
+    <h1 className="heading_style">List of Top 10 Netflix Series in All Time</h1>
+
+    <Card
+      
+      
+      imgsrc={Sdata[0].imgsrc}
+      title={Sdata[0].title}
+      sname={Sdata[0].sname}
+      links={Sdata[0].links} />
+
+    <Card
+      imgsrc={Sdata[1].imgsrc}
+      title={Sdata[1].title}
+      sname={Sdata[1].sname}
+      links={Sdata[1].links} />
+
+
+    <Card
+      imgsrc={Sdata[2].imgsrc}
+      title={Sdata[2].title}
+      sname={Sdata[2].sname}
+      links={Sdata[2].links} />
+
+<Card
+      imgsrc={Sdata[3].imgsrc}
+      title={Sdata[3].title}
+      sname={Sdata[3].sname}
+      links={Sdata[3].links} />
+      <Card
+      imgsrc={Sdata[4].imgsrc}
+      title={Sdata[4].title}
+      sname={Sdata[4].sname}
+      links={Sdata[4].links} />
+
+<Card
+      imgsrc={Sdata[5].imgsrc}
+      title={Sdata[5].title}
+      sname={Sdata[5].sname}
+      links={Sdata[5].links} />
+
+<Card
+      imgsrc={Sdata[6].imgsrc}
+      title={Sdata[6].title}
+      sname={Sdata[6].sname}
+      links={Sdata[6].links} />
+
+  </>
+  
+  ,
   document.getElementById('root')
 );
-
 
